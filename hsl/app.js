a328a1b304ec8e9f0be8e8ebe900897776e03794
Rene <rene@karkkainen.net>
2023-10-07 21:26:01 +0300

    Stops and route numbers

--- hsl/app.js
+++ hsl/app.js
@@ -6,11 +6,12 @@ const mqtt = require("mqtt");
 const client = mqtt.connect("mqtts://mqtt.hsl.fi:8883/");
 const WebSocket = require('ws');
 const http = require('http');
+require('dotenv').config();
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('WebSocket Server Running');
-  });
+});
 
 const wss = new WebSocket.Server({ server });
 
@@ -28,31 +29,80 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function broadcastData(data) {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data));
+wss.on('connection', async (ws) => {
+    console.log('A client connected');
+
+    ws.on('message', async (message) => {
+        try {
+            const requestData = JSON.parse(message); // Use a different variable name
+            switch (requestData.type) {
+                case "getStops":
+                    const stops = await getStops(requestData.latlon);
+                    ws.send(JSON.stringify({ type: "stops", data: stops }));
+                    break;
+            }
+        } catch (error) {
+            console.error('Error processing message:', error);
         }
     });
-}
+    
+    
+
+    // Handle client disconnection
+    ws.on('close', () => {
+        console.log('A client disconnected');
+    });
+});
 
 client.on("connect", () => {
     client.subscribe("/hfp/v2/journey/#", (err) => {
-        if (!err) {
-
-        }
     });
 });
 
 client.on("message", (topic, message) => {
     // message is Buffer, convert it to a string
     const messageString = message.toString();
-    broadcastData(messageString);
+    broadcastData("position", messageString);
 });
+
+function broadcastData(type, data) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({type: type, data: data}));
+        }
+    });
+}
+
+async function getStops(latlon) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/graphql',
+            'digitransit-subscription-key': process.env.API_KEY,
+        },
+        body: `{ stopsByRadius(lat:${latlon.lat}, lon:${latlon.lng}, radius:10000) { edges { node { stop { gtfsId name lat lon } distance } } } }`, // Convert the query to a JSON string
+    };
+
+    // Make the GraphQL query using fetch and return the result
+    try {
+        const response = await fetch('https://api.digitransit.fi/routing/v1/routers/finland/index/graphql', requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.data.stopsByRadius.edges;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Re-throw the error to handle it higher up the call stack if needed
+    }
+}
+
 
 const port = process.env.PORT || 3030;
 server.listen(port, () => {
-  console.log(`WebSocket server are running on port ${port}`);
+    console.log(`WebSocket server are running on port ${port}`);
 });
 
 module.exports = app;
--- hsl/public/javascripts/webmap.js
+++ hsl/public/javascripts/webmap.js
@@ -14518,40 +14518,108 @@ const socket = new WebSocket('ws://localhost:3030'); // Connect to a WS server t
 
 socket.addEventListener('open', (event) => {
     console.log('Connected to the WebSocket server');
+    socket.send(JSON.stringify({ type: "getStops", latlon: map.getCenter() }));
 });
 
-const markers = new Map(); // Create a map to store postions so that they can be updated
+// Define constants for map center and zoom
+const MAP_CENTER_KEY = 'mapCenter';
+const MAP_ZOOM_KEY = 'mapZoom';
 
+let stops;
+const addedStops = new Map();
+
+// Initialize markers object to store vehicle markers
+const markers = new Map();
+
+// Define a function to update markers
+function updateMarker(data) {
+    const { veh, lat, long, desi, spd } = data;
+    const position = [lat, long];
+
+    if (!map.getBounds().contains(position)) {
+        return; // Marker is out of bounds, no need to update or create it
+    }
+
+    let marker = markers.get(veh);
+
+    if (!marker) {
+        // Create a new marker if it doesn't exist
+        const circleMarker = L.circleMarker(position, {
+            radius: 20, // Adjust the radius to your desired size
+        }).addTo(map);
+
+        circleMarker.bindPopup(`Route: ${desi}, Speed: ${spd * 3.6}`);
+
+        const textLabel = L.divIcon({
+            className: 'text-label',
+            html: `<p>${desi}</p>`,
+            iconSize: [20, 20],
+            iconAnchor: [10, 20],
+        });
+
+        const textMarker = L.marker(position, {
+            icon: textLabel,
+        }).addTo(map);
+
+        textMarker.bindPopup(`Route: ${desi}, Speed: ${spd * 3.6}`);
+
+        marker = { circleMarker, textMarker };
+        markers.set(veh, marker);
+    } else {
+        // Update existing marker's position and popup content
+        marker.circleMarker.setLatLng(position);
+        marker.textMarker.setLatLng(position);
+        marker.circleMarker.getPopup().setContent(`Route: ${desi}, Speed: ${spd * 3.6}`);
+        marker.textMarker.getPopup().setContent(`Route: ${desi}, Speed: ${spd * 3.6}`);
+    }
+}
+
+// Add an event listener for WebSocket messages
 socket.addEventListener('message', (event) => {
-    const data = JSON.parse(event.data);
-    const vpData = JSON.parse(data).VP;
-
     try {
-        const { veh, lat, long, desi, spd } = vpData; // Destructure the properties for convenience
-        const position = [lat, long]; // Create an array for the position
-        const marker = markers.get(veh); // Get the marker for this vehicle number
+        const data = JSON.parse(event.data);
+        console.log(data.type);
 
-        // Check if a marker for this vehicle number already exists
-        if (marker) {
-            // Check if the marker is within the current map bounds
-            if (map.getBounds().contains(position)) {
-                // Update the existing marker's position and popup content
-                marker.setLatLng(position);
-                marker.getPopup().setContent(`Route: ${desi}, Speed: ${spd * 10}`);
-            }
-        } else {
-            // Check if the position is within the current map bounds
-            if (map.getBounds().contains(position)) {
-                // Create a new marker and add it to the map and the Map object
-                const circleMarker = L.circleMarker(position, {
-                    radius: 10, // Adjust the radius to your desired size
-                }).addTo(map);
-                circleMarker.bindPopup(`Route: ${desi}, Speed: ${spd * 10}`);
-                markers.set(veh, circleMarker); // Store the marker using the vehicle number as the key
-            }
+        switch (data.type) {
+            case "position":
+                const vpData = JSON.parse(data.data).VP;
+                if (vpData) {
+                    updateMarker(vpData);
+                    // Store map center and zoom in local storage
+                    localStorage.setItem(MAP_CENTER_KEY, JSON.stringify(map.getCenter()));
+                    localStorage.setItem(MAP_ZOOM_KEY, map.getZoom());
+                }
+                break;
+            case "stops":
+                // Remove old stops
+                for (const [gtfsId, marker] of addedStops.entries()) {
+                    map.removeLayer(marker);
+                    addedStops.delete(gtfsId);
+                }
+
+                // Add new stops
+                for (const nodedata of data.data) {
+                    const stop = nodedata.node.stop;
+                    const latLng = L.latLng(stop.lat, stop.lon);
+
+                    // Check if the stop is within the current map viewport
+                    if (!addedStops.has(stop.gtfsId)) {
+                        const marker = L.circleMarker(latLng, {
+                            radius: 5,
+                            color: 'red',
+                        }).addTo(map);
+                        marker.bindPopup(stop.name);
+
+                        // Mark this stop as added in the Map
+                        addedStops.set(stop.gtfsId, marker);
+                    }
+                }
+
+                break;
+            default:
+                break;
         }
-        localStorage.setItem('mapCenter', JSON.stringify(map.getCenter()));
-        localStorage.setItem('mapZoom', map.getZoom());
+
     } catch (err) {
         // Handle any errors
         //console.error(err);
@@ -14566,8 +14634,11 @@ const savedMapZoom = parseInt(localStorage.getItem('mapZoom'));
 var map = L.map("map", {
     center: savedMapCenter || [60.1699, 24.9384], // Use saved center or default
     zoom: savedMapZoom || 13, // Use saved zoom level or default
-  });
+});
 
+map.on('moveend', () => {
+    socket.send(JSON.stringify({ type: "getStops", latlon: map.getCenter() }));
+});
 
 socket.addEventListener('close', (event) => {
     console.log('Connection closed');
@@ -14576,9 +14647,8 @@ socket.addEventListener('close', (event) => {
 // Adds the basemap tiles to your web map
 L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
     attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: "abcd",
     maxZoom: 21,
 }).addTo(map);
-
 },{"leaflet":1}]},{},[2]);
